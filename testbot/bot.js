cce64eb4d33e2d05e26e9e6911f8a28c6b9f4cba
Paul Acklam <paulacklam@gmail.com>
2018-10-31 18:25:23 +0000

    add userState

--- testbot/bot.js
+++ testbot/bot.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const { ActivityTypes } = require('botbuilder');
+const { ActivityTypes, BotState } = require('botbuilder');
 const { LuisRecognizer } = require('botbuilder-ai');
 
 const yelpConfig = {
@@ -9,25 +9,68 @@ const yelpConfig = {
 }
 
 class LuisBot {
-  constructor(application, luisPredictionOptions) {
+  constructor(userState, application, luisPredictionOptions) {
+    this.userState = userState;
+    this.userCuisine = userState.createProperty('cuisine');
+    this.userLocation = userState.createProperty('location');
     this.luisRecognizer = new LuisRecognizer(application, luisPredictionOptions, true);
+    // console.log(Object.getOwnPropertyNames(userState))
+    // console.log(userState.clear)
   }
 
   async onTurn(turnContext) {
     if (turnContext.activity.type === ActivityTypes.Message) {
       const results = await this.luisRecognizer.recognize(turnContext);
       console.log(results)
+      
+      console.log('Cuisine: ' + await this.userCuisine.get(turnContext))
+      console.log('Location: ' + await this.userLocation.get(turnContext))
 
-      const cuisine = results.luisResult.entities.filter(entity => entity.type === 'cuisine')[0].entity
-      const location = results.luisResult.entities.filter(entity => entity.type === 'location')[0].entity
+      let cuisine = await this.userCuisine.get(turnContext)
+      let location = await this.userLocation.get(turnContext)
 
-      await turnContext.sendActivity(`Sounds like you're looking for a ${cuisine} restaurant in ${location}...`);
+      try {
+        cuisine = results.luisResult.entities.filter(entity => entity.type === 'cuisine')[0].entity
+        await this.userCuisine.set(turnContext, cuisine)
+      } catch(error) {
+        console.log(error)
+      }
 
-      await this.getBusinesses(cuisine, location)
-      .then(businesses => turnContext.sendActivity(`How about ${businesses[0].name} in ${businesses[0].location.city}`))
-      .catch(error => turnContext.sendActivity(`${error}`))
+      try {
+        location = results.luisResult.entities.filter(entity => entity.type === 'location')[0].entity
+        await this.userLocation.set(turnContext, location)
+      } catch(error) {
+        console.log(error)
+      }
 
+      if (!location) {
+        await turnContext.sendActivity(`Where abouts do you want to look for a ${cuisine} restaurant?`);  
+      }
 
+      if (!cuisine) {
+        await turnContext.sendActivity(`What type of restaurant are you looking for in ${location}?`);  
+      }
+      
+      console.log('Cuisine: ' + await this.userCuisine.get(turnContext))
+      console.log('Location: ' + await this.userLocation.get(turnContext))
+
+      
+      if (location && cuisine) {
+        await turnContext.sendActivity(`Sounds like you're looking for a ${cuisine} restaurant in ${location}...`);
+        
+        await this.getBusinesses(cuisine, location)
+        .then(businesses => turnContext.sendActivity(`How about ${businesses[0].name} in ${businesses[0].location.city}?`))
+        .catch(error => turnContext.sendActivity(`${error}`))
+        
+        await this.userState.clear(turnContext);
+        
+        console.log('Cuisine: ' + await this.userCuisine.get(turnContext))
+        console.log('Location: ' + await this.userLocation.get(turnContext))
+      }
+      
+      // save the userState
+      await this.userState.saveChanges(turnContext);
+    
     // this bit does the welcome text
     } else if (turnContext.activity.type === ActivityTypes.ConversationUpdate &&
       turnContext.activity.recipient.id !== turnContext.activity.membersAdded[0].id) {
--- testbot/index.js
+++ testbot/index.js
@@ -1,10 +1,10 @@
 const restify = require('restify');
-const botbuilder = require('botbuilder');
+const { BotFrameworkAdapter, MemoryStorage, UserState } = require('botbuilder');
 const { LuisBot } = require('./bot');
 const luisApplication = require('./luisConfig');
  
 // Create bot adapter, which defines how the bot sends and receives messages.
-var adapter = new botbuilder.BotFrameworkAdapter({
+var adapter = new BotFrameworkAdapter({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword
 });
@@ -15,14 +15,17 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log(`\n${server.name} listening to ${server.url}`);
 });
 
-
 const luisPredictionOptions = {
     includeAllIntents: true,
     log: true,
     staging: false
 };
 
-const bot = new LuisBot(luisApplication, luisPredictionOptions);
+// Define state store for your bot.
+const memoryStorage = new MemoryStorage();
+const userState = new UserState(memoryStorage);
+
+const bot = new LuisBot(userState, luisApplication, luisPredictionOptions);
 
 // Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
