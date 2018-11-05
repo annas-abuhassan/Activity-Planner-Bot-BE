af14fd32250940f93cb168bbcbefc5300d51c3db
Annas Abu-Hassan <annas.abuhassan@gmail.com>
2018-11-05 08:47:51 +0000

    brought in config data for fb/yelp/luis into one config.js file

---
+++ testbot/config.js
@@ -0,0 +1,14 @@
+const config = {
+  luisApplication: {
+    applicationId: '6ac3b6dc-431c-4dd6-a659-0f270f8674c4',
+    endpointKey: '2e790269706e4920bbb7ffd82a29dc1d',
+    endpoint:
+      'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/6ac3b6dc-431c-4dd6-a659-0f270f8674c4?subscription-key=2e790269706e4920bbb7ffd82a29dc1d&timezoneOffset=-360&q='
+  },
+  yelpKey:
+    'Bearer 8SHw5ycrrXc7JHrLrJ5vA2Wm1HjuH6O-IHJ2w7TTs1anxi5JeQkX1wopZNFT6TuSf3vwysZz1xIeAmNbIPkewghISlejBB5FgZgAhtxsk7JzLkZFBHEi-toawXTYW3Yx',
+  facebookToken:
+    'EAAKVOuZBgYjEBANZBZAHCzbq7LBO9btFL4AYZAMbLjO4oqZBpV3ZAWUQgLIFwX1d8OSoDmPB6OFN862eZAfsGX90dXRItQyToPZBEJ4bLlVcEH6DUZAWZCvCi5VS1sQ0vIT5mbYII9UEAnJTCRa61gJi8OearZAKmZA64iTnXoIsPLuEuAZDZD'
+};
+
+module.exports = config;
