4fa885c6d052c3d26427571658f5979d7e3cd3be
Annas Abu-Hassan <annas.abuhassan@gmail.com>
2018-11-05 11:51:27 +0000

    changed luisApplication config reference

--- testbot/index.js
+++ testbot/index.js
@@ -1,25 +1,24 @@
 const restify = require('restify');
 const { BotFrameworkAdapter, MemoryStorage, UserState } = require('botbuilder');
 const { LuisBot } = require('./bot');
-// const luisApplication = require('./luisConfig');
-const luisApplication = require('./luisConfig2');
- 
+const { luisApplication } = require('./config');
+
 // Create bot adapter, which defines how the bot sends and receives messages.
 var adapter = new BotFrameworkAdapter({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword
+  appId: process.env.MicrosoftAppId,
+  appPassword: process.env.MicrosoftAppPassword
 });
- 
+
 // Create HTTP server.
 let server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log(`\n${server.name} listening to ${server.url}`);
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+  console.log(`\n${server.name} listening to ${server.url}`);
 });
 
 const luisPredictionOptions = {
-    includeAllIntents: true,
-    log: true,
-    staging: false
+  includeAllIntents: true,
+  log: true,
+  staging: false
 };
 
 // Define state store for your bot.
@@ -30,7 +29,7 @@ const bot = new LuisBot(userState, luisApplication, luisPredictionOptions);
 
 // Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async(turnContext) => {
-        await bot.onTurn(turnContext);
-    });
+  adapter.processActivity(req, res, async turnContext => {
+    await bot.onTurn(turnContext);
+  });
 });
