88b7e22dd3267671a7f2f7ad49288168b357df11
Annas Abu-Hassan <annas.abuhassan@gmail.com>
2018-11-05 08:43:16 +0000

    Brought in function that pushes cards to fb

--- testbot/utils.js
+++ testbot/utils.js
@@ -1,18 +1,17 @@
 const axios = require('axios');
-const accessToken =
-  'EAAKVOuZBgYjEBANZBZAHCzbq7LBO9btFL4AYZAMbLjO4oqZBpV3ZAWUQgLIFwX1d8OSoDmPB6OFN862eZAfsGX90dXRItQyToPZBEJ4bLlVcEH6DUZAWZCvCi5VS1sQ0vIT5mbYII9UEAnJTCRa61gJi8OearZAKmZA64iTnXoIsPLuEuAZDZD';
+const { facebookToken } = require('./config');
 
 const getUserData = userId => {
   return axios
     .get(
-      `https://graph.facebook.com/${userId}?fields=first_name,last_name,profile_pic&access_token=${accessToken}`
+      `https://graph.facebook.com/${userId}?fields=first_name,last_name,profile_pic&access_token=${facebookToken}`
     )
     .then(({ data }) => data);
 };
 
 const sendLocation = userId => {
   return axios.post(
-    `https://graph.facebook.com/v2.6/me/messages?access_token=${accessToken}`,
+    `https://graph.facebook.com/v2.6/me/messages?access_token=${facebookToken}`,
     {
       recipient: {
         id: `${userId}`
@@ -29,4 +28,66 @@ const sendLocation = userId => {
   );
 };
 
-module.exports = { getUserData, sendLocation };
+const sendCard = (id, businesses) => {
+  const elementsArray = [];
+  businesses.forEach(business => {
+    const {
+      name,
+      image_url,
+      categories,
+      url,
+      display_phone,
+      rating,
+      price,
+      location
+    } = business;
+
+    const businessElement = {
+      title: name,
+      image_url: image_url,
+      subtitle: categories[0],
+      subtitle: location.display_address[0] + ', ' + location.zip_code + '.',
+      subtitle: price,
+      subtitle: rating,
+      default_action: {
+        type: 'web_url',
+        url: url,
+        webview_height_ratio: 'tall'
+      },
+      buttons: [
+        {
+          type: 'web_url',
+          url: url,
+          title: 'View Website'
+        },
+        {
+          type: 'phone_number',
+          title: 'Call to Book',
+          payload: display_phone
+        }
+      ]
+    };
+
+    elementsArray.push(businessElement);
+  });
+
+  return axios.post(
+    `https://graph.facebook.com/v2.6/me/messages?access_token=${facebookToken}`,
+    {
+      recipient: {
+        id: id
+      },
+      message: {
+        attachment: {
+          type: 'template',
+          payload: {
+            template_type: 'generic',
+            elements: elementsArray
+          }
+        }
+      }
+    }
+  );
+};
+
+module.exports = { getUserData, sendLocation, sendCard };
