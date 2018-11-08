a540dabb926355dc30b9bfb8a178025aa9eb30c0
Annas Abu-Hassan <annas.abuhassan@gmail.com>
2018-11-08 17:54:12 +0000

    added typing indicators for hi and help

--- testbot/bot.js
+++ testbot/bot.js
@@ -112,8 +112,16 @@ class LuisBot {
         } else if (intent === 'greetingIntent') {
           await this.greetUser(turnContext);
         } else if (intent === 'requestHelp') {
+          await sendTypingIndicator(
+            await this.userId.get(turnContext),
+            await this.userChannel.get(turnContext)
+          );
           await turnContext.sendActivity(
             'I can help you to find restaurants, bars, pubs and cafes anywhere in the world! 🌍'
+          );
+          await sendTypingIndicator(
+            await this.userId.get(turnContext),
+            await this.userChannel.get(turnContext)
           );
           await turnContext.sendActivity(`Try asking me things like:\n
           "Where can I go for cocktails in London? 🍹" or\n 
@@ -319,6 +327,10 @@ class LuisBot {
   async greetUser(turnContext) {
     const userName = await this.userName.get(turnContext);
     const randomGreeting = shuffle(['Hello', 'Hey', 'Hi', 'Howdy', 'Yo'])[0];
+    await sendTypingIndicator(
+      await this.userId.get(turnContext),
+      await this.userChannel.get(turnContext)
+    );
     await turnContext.sendActivity(`${randomGreeting} ${userName || ''}`);
   }
 }
